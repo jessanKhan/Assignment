6e4ca9f700ccabd2b6e9ec047664dcfd5cdc082a
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2020-11-07 23:07:58 +0600

    Add files via upload

---
+++ src/components/form.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import {Formik} from 'formik'
+import {TextField, Button} from '@material-ui/core'
+
+const initialValues={
+  project:"project",
+  description:"description",
+  client:"client",
+  contractor:'contractor'
+}
+
+const Forms = (props) => {
+
+
+
+    const [data,setData] =useState();
+    // const [description,setDescription]=useState('');
+    // const [client,setClient]=useState('');
+    // const [contractor,setContractor]=useState('');
+    const [step,setStep]=useState(1)
+
+   const  onsubmitfunction=(formData)=>{
+       console.log("e.target.value",formData)
+       setData(formData)
+       setStep(2)
+
+   }
+
+
+  return (
+    <div> 
+       {step ==1 && <Formik
+       initialValues={initialValues}
+       onSubmit={data=>onsubmitfunction(data)}>
+{({values,handleChange,handleBlur,handleSubmit})=>(
+  <form onSubmit={handleSubmit}>
+    <div>
+    <TextField name="project" 
+    value={values.project} 
+    onChange={handleChange} 
+    onBlur={handleBlur} />
+    </div><div>
+    <TextField name="description" 
+    value={values.description} 
+    onChange={handleChange} 
+    onBlur={handleBlur} />
+    </div>
+    <div>
+    <TextField name="client" 
+    value={values.client} 
+    onChange={handleChange} 
+    onBlur={handleBlur} />
+    </div>
+    <div>
+    <TextField name="contractor" 
+    value={values.contractor} 
+    onChange={handleChange} 
+    onBlur={handleBlur} />
+    </div>
+    <Button type="submit" >Submit</Button>
+  </form>
+)}
+
+
+       </Formik>}
+       {}
+    </div>
+  );
+};
+
+export default Forms;
