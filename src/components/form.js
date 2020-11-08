54282d9b8a7a75c70cc88f191896360e2309824b
torikul <torikul>
2020-11-08 22:47:03 +0600

    sdf

--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Formik } from "formik";
 import { TextField, Button } from "@material-ui/core";
-import Papa,{CSVReader} from 'papaparse';
+import Papa, { CSVReader } from 'papaparse';
 import * as XLSX from "xlsx";
-import {csv} from "d3";
+import { csv } from "d3";
 import file from '../file/input_values.csv';
 
 const initialValues = {
@@ -35,15 +35,15 @@ const Forms = (props) => {
   //     fileReader.readAsArrayBuffer(file[0]);
 
   //     fileReader.onload = async (e) => {
-    
+
   //       const bufferArray = e.target.result;
 
   //       const wb = XLSX.read(bufferArray, { type: "buffer" });
-        
+
   //       const wsname = wb.SheetNames[0];
-        
+
   //       const ws = wb.Sheets[wsname];
-        
+
   //       const data = XLSX.utils.sheet_to_json(ws);
   //       console.log("onload file",ws)
   //       resolve(data);
@@ -62,19 +62,22 @@ const Forms = (props) => {
 
 
   const showFile = async (e) => {
-    console.log(e.target.files[0].path)
-    csv(file).then(data=> console.log(data,"csv data"))
+    const localUrl = URL.createObjectURL(e.target.files[0]);
+    csv(localUrl).then(data => console.log(data, "csv data"))
     setItems(data)
+
+
+
     // const parsed =Papa.parse(e.target.files[0])
     // e.preventDefault()
     // const reader = new FileReader()
     // reader.onload = async (e) => { 
     //   const text = (e.target.result)
-      
+
     //   const datas= JSON.stringify(text)
     //   console.log(datas)
     //   alert(datas)
-     
+
     // };
     // reader.readAsText(e.target.files[0])
     // console.log(e.target.files)
@@ -135,19 +138,19 @@ const Forms = (props) => {
           <div>{data.client}</div>
           <div>{data.contractor}</div>
 
-      
+
           <div>
             <Button variant="contained" component="label">
               Upload File
               <input type="file" onChange={
-          showFile
-        } style={{ display: "none" }} />
+                showFile
+              } style={{ display: "none" }} />
             </Button>
           </div>
 
-          {items.length>0 && 
-          items.map(<div>{items.KP}</div>
-         )}
+          {items.length > 0 &&
+            items.map(<div>{items.KP}</div>
+            )}
         </div>
       )}
     </div>
